ad5c59e5e6d31554e1009a514644cf17b8d2f714
srivedhbalguri <62438630+srivedhbalguri@users.noreply.github.com>
2021-03-02 16:51:31 -0500

    css

--- checkout.php
+++ checkout.php
@@ -15,7 +15,7 @@ if (isset($_SESSION['selectedBook']))
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Conestoga Books</title>
     <link rel="stylesheet" href="css/project.css">
-    <link rel="stylesheet" href="css/checkout.css">
+    <!-- <link rel="stylesheet" href="css/checkout.css"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
